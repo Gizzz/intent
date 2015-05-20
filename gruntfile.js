5e3e6507a8883175397a3aaa440a5297738902d2
Gizzz <bulatkhusainov@yandex.ru>
2015-05-20 13:57:12 +0300

    project structure changed (dev and dist editions)

--- gruntfile.js
+++ gruntfile.js
@@ -1,43 +1,66 @@
+'use strict';
+
 module.exports = function(grunt) {
     grunt.initConfig({
         less: {
             dev: {
                 options: {
-                    paths: ["css", "less"]
+                    paths: ["dev/styles"],
+                    sourceMap: true,
+                    sourceMapFileInline: true,
                 },
-                files: {
-                    "css/final.css": "less/final.less"
-                }
+                src: 'dev/styles/style.less',
+                dest: 'dev/styles/style.css'
             }
         },
+        // autoprefixer: {
+        //     dev: {
+        //         options: {
+        //             browsers: ['last 2 versions'],
+        //             map: true,
+        //         },
+        //         src: 'css/final-unprefixed.css',
+        //         dest: 'css/final.css'
+        //     }
+        // },
         watch: {
-            files: ['less/*.less'],
-            tasks: ['less']
+            less: {
+                files: ['dev/styles/*.less'],
+                tasks: ['less']
+            },
+            // autoprefixer: {
+            //     files: ['css/final-unprefixed.css'],
+            //     tasks: ['autoprefixer']
+            // },
         },
         browserSync: {
             bsFiles: {
                 src: [
-                    '*.html',
-                    'css/*.css',
-                    'js/**/*.js'
+                    'dev/**/*.*'
                 ]
             },
             options: {
                 watchTask: true,
                 server: {
-                    baseDir: "./"
+                    baseDir: "./dev/"
                 }
             }
         },
     });
 
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-autoprefixer');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
 
-    grunt.registerTask('default', [
+    grunt.registerTask('dev', [
         'less',
+        // 'autoprefixer',
         'browserSync',
         'watch',
+    ]);
+
+    grunt.registerTask('default', [
+        'dev',
     ]);
 };
