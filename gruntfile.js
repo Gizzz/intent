ae7020de3cbafd6c2cca04b900d94ed790a59eb8
Gizzz <bulatkhusainov@yandex.ru>
2015-05-21 18:19:01 +0300

    build process update
    dist renamed to prod,
    prod tasks added

--- gruntfile.js
+++ gruntfile.js
@@ -11,27 +11,22 @@ module.exports = function(grunt) {
                 },
                 src: 'dev/styles/style.less',
                 dest: 'dev/styles/style.css'
+            },
+            prod: {
+                options: {
+                    paths: ["dev/styles"],
+                },
+                src: 'dev/styles/style.less',
+                dest: 'prod/styles/style.css'
             }
         },
-        // autoprefixer: {
-        //     dev: {
-        //         options: {
-        //             browsers: ['last 2 versions'],
-        //             map: true,
-        //         },
-        //         src: 'css/final-unprefixed.css',
-        //         dest: 'css/final.css'
-        //     }
-        // },
         watch: {
-            less: {
-                files: ['dev/styles/*.less'],
-                tasks: ['less']
-            },
-            // autoprefixer: {
-            //     files: ['css/final-unprefixed.css'],
-            //     tasks: ['autoprefixer']
-            // },
+            dev: {
+                less: {
+                    files: ['dev/styles/*.less'],
+                    tasks: ['less']
+                },
+            }
         },
         browserSync: {
             bsFiles: {
@@ -46,18 +41,70 @@ module.exports = function(grunt) {
                 }
             }
         },
-    });
+        clean: {
+            prod: {
+                src: ["prod"]
+            }
+        },
+        copy: {
+            prod: {
+                expand: true,
+                cwd: 'dev/',
+                src: [
+                    'img/**',
+                    'pict/**',
 
-    grunt.loadNpmTasks('grunt-contrib-less');
+                    'fonts/**',
+                    'js/**',
+                    'favicon.ico',
+                    '*.html'
+                ],
+                dest: 'prod/',
+            }
+        },
+        autoprefixer: {
+            dev: {
+                options: {
+                    browsers: ['last 2 versions'],
+                },
+                src:  'prod/styles/style.css',
+                dest: 'prod/styles/style.css',
+            }
+        },
+        cssmin: {
+            prod: {
+                files: [{
+                    expand: true,
+                    cwd: 'prod/styles/',
+                    src:  'style.css',
+                    dest: 'prod/styles/',
+                }]
+            }
+        },
+    });
+    
     grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('dev', [
-        'less',
-        // 'autoprefixer',
+        'less:dev',
         'browserSync',
         'watch',
+    ]);
+
+    grunt.registerTask('prod', [
+        'clean',
+        'copy',
+        'less:prod',
+        'autoprefixer',
+        'cssmin'
     ]);
 
     grunt.registerTask('default', [
